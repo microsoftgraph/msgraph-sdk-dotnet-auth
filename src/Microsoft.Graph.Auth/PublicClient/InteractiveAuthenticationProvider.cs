9fa88faf7b7d00e08624873c749d2d1d0ebd1784
Sidney Andrews <sidney@seesharprun.net>
2019-05-16 17:24:47 -0400

    Added build conditional directives

--- src/Microsoft.Graph.Auth/PublicClient/InteractiveAuthenticationProvider.cs
+++ src/Microsoft.Graph.Auth/PublicClient/InteractiveAuthenticationProvider.cs
@@ -10,6 +10,9 @@ namespace Microsoft.Graph.Auth
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
+#if NET45
+    using System.Windows.Forms;
+#endif
 
     /// <summary>
     /// An <see cref="IAuthenticationProvider"/> implementation using MSAL.Net to acquire token interactively
@@ -21,12 +24,49 @@ namespace Microsoft.Graph.Auth
         /// </summary>
         public Prompt Prompt { get; set; }
 
+#if NET45
         /// <summary>
         /// Parent activity or window.
         /// </summary>
-        /// <remarks>
-        /// Typically used in Xamarin projects.
-        /// </remarks>
+        [CLSCompliant(false)]
+        public IWin32Window ParentWindow { get; set; }
+
+        /// <summary>
+        /// Parent activity or window.
+        /// </summary>
+        [CLSCompliant(false)]
+        public IntPtr ParentPointer { get; set; }
+
+        /// <summary>
+        /// Constructs a new <see cref="InteractiveAuthenticationProvider"/>
+        /// </summary>
+        /// <param name="publicClientApplication">A <see cref="IPublicClientApplication"/> to pass to <see cref="DeviceCodeProvider"/> for authentication.</param>
+        /// <param name="scopes">Scopes required to access Microsoft Graph. This defaults to https://graph.microsoft.com/.default when none is set.</param>
+        /// <param name="prompt">Designed interactive experience for the user. Defaults to <see cref="Prompt.SelectAccount"/>.</param>
+        /// <param name="parent">Object containing a reference to the parent window/activity.</param>
+        [CLSCompliant(false)]
+        public InteractiveAuthenticationProvider(
+            IPublicClientApplication publicClientApplication,
+            IEnumerable<string> scopes = null,
+            Prompt? prompt = null,
+            IWin32Window window = null,
+            IntPtr pointer = default(IntPtr))
+            : base(scopes)
+        {
+            ClientApplication = publicClientApplication ?? throw new AuthenticationException(
+                    new Error
+                    {
+                        Code = ErrorConstants.Codes.InvalidRequest,
+                        Message = string.Format(ErrorConstants.Message.NullValue, nameof(publicClientApplication))
+                    });
+            Prompt = prompt ?? Prompt.SelectAccount;
+            ParentWindow = window;
+            ParentPointer = pointer;
+        }
+#elif NETSTANDARD1_3
+        /// <summary>
+        /// Parent activity or window.
+        /// </summary>
         public object Parent { get; set; }
 
         /// <summary>
@@ -52,7 +92,7 @@ namespace Microsoft.Graph.Auth
             Prompt = prompt ?? Prompt.SelectAccount;
             Parent = parent;
         }
-
+#endif
         /// <summary>
         /// Creates a new <see cref="IPublicClientApplication"/>.
         /// </summary>
@@ -118,14 +158,25 @@ namespace Microsoft.Graph.Auth
             {
                 try
                 {
-                    authenticationResult = await ClientApplication.AcquireTokenInteractive(scopes)
+                    var builder = ClientApplication.AcquireTokenInteractive(scopes)
                         .WithAccount(account)
                         .WithPrompt(Prompt)
                         .WithExtraQueryParameters(extraQueryParameter)
                         .WithExtraScopesToConsent(null)
-                        .WithAuthority(ClientApplication.Authority)
-                        .WithParentActivityOrWindow(Parent)
-                        .ExecuteAsync();
+                        .WithAuthority(ClientApplication.Authority);
+#if NET45
+                    if (ParentWindow != null)
+                    {
+                        builder = builder.WithParentActivityOrWindow(ParentWindow);
+                    }
+                    else
+                    {
+                        builder = builder.WithParentActivityOrWindow(ParentPointer);
+                    }
+#elif NETSTANDARD1_3
+                    builder = builder.WithParentActivityOrWindow(Parent);
+#endif
+                    authenticationResult = await builder.ExecuteAsync();
                     break;
                 }
                 catch (MsalServiceException serviceException)
