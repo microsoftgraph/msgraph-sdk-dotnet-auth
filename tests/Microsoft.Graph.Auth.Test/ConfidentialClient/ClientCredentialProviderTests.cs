997ffa0b1daf0d710d9640b63f025f16165fd959
Firomsa Abdurahman <v-fiabd@microsoft.com>
2019-07-22 11:04:54 -0700

    Added Optional Scope parameter to ClientCredentialProvider

--- tests/Microsoft.Graph.Auth.Test/ConfidentialClient/ClientCredentialProviderTests.cs
+++ tests/Microsoft.Graph.Auth.Test/ConfidentialClient/ClientCredentialProviderTests.cs
@@ -29,6 +29,27 @@
         }
 
         [Fact]
+        public void ConstructorShouldUseDefualtScopeWhenScopeisNull()
+        {
+            string clientId = "00000000-0000-0000-0000-000000000000";
+            string clientSecret = "00000000-0000-0000-0000-000000000000";
+            string authority = "https://login.microsoftonline.com/organizations/";
+            string redirectUri = "https://login.microsoftonline.com/common/oauth2/deviceauth";
+
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithRedirectUri(redirectUri)
+                .WithClientSecret(clientSecret)
+                .WithAuthority(authority)
+                .Build();
+
+            ClientCredentialProvider auth = new ClientCredentialProvider(confidentialClientApplication, null);
+
+            Assert.NotNull(auth.Scope);
+            Assert.Equal(AuthConstants.DefaultScopeUrl, auth.Scope);
+        }
+
+        [Fact]
         public void ConstructorShouldThrowExceptionWithNullConfidentialClientApp()
         {
             AuthenticationException ex = Assert.Throws<AuthenticationException>(() => new ClientCredentialProvider(null));
